5f06bcffc67781de7f5983c38b36c6c4f1a9de9a
MarcoTonyDiaz <sickro_warco@comunidad.unam.mx>
2018-07-24 18:36:15 -0500

    cambio de formato y tipo de boton a input

--- Sintco.php
+++ Sintco.php
@@ -1,8 +1,9 @@
-<div style="position: fixed; top: 50%; right: 50px;">
+
+<html><div style="position: fixed; top: 50%; right: 50px;">
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input name="cmd" type="hidden" value="_s-xclick" /><br />
 <input name="hosted_button_id" type="hidden" value="VYRFAUW9KH7R2" /><br />
-<button type="submit" value="Quiero donar" name="submit" title="PayPal -
+<input type="submit" value="Quiero donar" name="submit" title="PayPal -
 The safer, easier way to pay online!" class="paypal_btn"><i class="fas fa-hand-holding-usd"></i></button><br />
 <img src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form>
 <style>
@@ -21,10 +22,11 @@ cursor: pointer;
 outline: none;
 border-radius:30px;
 }
-.paypal_btn:hover{ background: #e2017e; }
+.paypal_btn:hover{ background:#fff ;
+color:#e2147e }
 </style>
 </div>
-
+<!--
 /*OTRO*/
 
 [md_button
@@ -55,3 +57,4 @@ display: none;
 button_size="small" left_right_padding="30" button_align="right"
 button_icon_class="icon-banking-donation-22" button_color="rgb(226,20,126)"
 button_url="#Quierohacerunadonacion"]</div>
+-->
